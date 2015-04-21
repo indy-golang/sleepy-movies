bf53c698dfeef0ee60edfbadbdca17d32db14329
Larry Price <larry.price.dev@gmail.com>
2015-04-21 17:36:43 -0400

    /movies

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
-import "github.com/dougblack/sleepy"
+import (
+	"github.com/dougblack/sleepy"
+	"gopkg.in/mgo.v2"
+  "fmt"
+)
 
 func main() {
-  movie := new(Movie)
+	populateMovies()
+	movie := new(Movie)
 
-  api := sleepy.NewAPI()
-  api.AddResource(movie, "/movies")
-  api.Start(3000)
+	api := sleepy.NewAPI()
+	api.AddResource(movie, "/movies")
+	fmt.Println(api.Start(3000))
+}
+
+func populateMovies() {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	c := session.DB("sleepy-movies").C("movies")
+  c.DropCollection()
+	c.Insert(Movie{
+		Title: "The Double",
+	})
+	c.Insert(Movie{
+		Title: "Ferngully",
+	})
+	c.Insert(Movie{
+		Title: "Clue",
+	})
 }
--- movie.go
+++ movie.go
@@ -1,22 +1,36 @@
 package main
 
 import (
-  "net/url"
-  "net/http"
-  "fmt"
+	"net/http"
+	"net/url"
+	"gopkg.in/mgo.v2"
 )
 
-type Movie struct { /* empty */ }
+type Movie struct {
+	Title       string
+	Genre       string
+	Released    string
+	Description string
+	Cast        []string
+}
+
+func (Movie) coll() *mgo.Collection {
+  session, err := mgo.Dial("localhost")
+  if err != nil {
+    panic(err)
+  }
+  return session.DB("sleepy-movies").C("movies")
+}
 
 func (movie Movie) Get(values url.Values, headers http.Header) (int, interface{}, http.Header) {
-  movies := []string{"Gone with the Wind", "The Manchurian Candidate"}
-  data := map[string][]string{"movies": movies}
-  return 200, data, http.Header{"Content-type": {"application/json"}}
+	var movies []Movie
+  movie.coll().Find(nil).All(&movies)
+	data := map[string][]Movie{"movies": movies}
+	return 200, data, http.Header{"Content-type": {"application/json"}}
 }
 
 func (movie Movie) Post(values url.Values, headers http.Header) (int, interface{}, http.Header) {
-  fmt.Println(values)
-  movies := []string{"Gone with the Wind", "The Manchurian Candidate"}
-  data := map[string][]string{"movies": movies}
-  return 200, data, http.Header{"Content-type": {"application/json"}}
+	movies := []string{"Gone with the Wind", "The Manchurian Candidate"}
+	data := map[string][]string{"movies": movies}
+	return 200, data, http.Header{"Content-type": {"application/json"}}
 }
